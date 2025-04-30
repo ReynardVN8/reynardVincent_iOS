922b1837f96a1b94ef00d11ad6273a8253a2d92a
Reynard Vincent <reynardvn8@gmail.com>
2025-04-30 23:42:15 +0700

    Adding Initial files

---
+++ reynardVincent_iOS/reynardVincent_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  reynardVincent_iOS
+//
+//  Created by ReynardVN8 on 30/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ reynardVincent_iOS/reynardVincent_iOS/reynardVincent_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  reynardVincent_iOSApp.swift
+//  reynardVincent_iOS
+//
+//  Created by ReynardVN8 on 30/04/25.
+//
+
+import SwiftUI
+
+@main
+struct reynardVincent_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
