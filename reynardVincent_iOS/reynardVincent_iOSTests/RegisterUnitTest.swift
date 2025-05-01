81c69f126f6d2a363ea265d5d08385eb353058f2
Reynard Vincent <reynardvn8@gmail.com>
2025-05-01 13:31:26 +0700

    Added unit testing templates

--- reynardVincent_iOS/reynardVincent_iOSTests/RegisterUnitTest.swift
+++ reynardVincent_iOS/reynardVincent_iOSTests/RegisterUnitTest.swift
@@ -1,7 +1,16 @@
 //
 //  RegisterUnitTest.swift
-//  reynardVincent_iOS
+//  reynardVincent_iOSTests
 //
 //  Created by ReynardVN8 on 01/05/25.
 //
 
+import Testing
+
+struct RegisterUnitTest {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
